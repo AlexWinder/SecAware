89069c00d16b382998389ce9b190c372e672331f
Alex Winder <contact@alexwinder.com>
2026-02-20 00:01:48 +0200

    Start of tool

---
+++ SecAware.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import dotenv
+import os
+import sys
+
+def checkDotEnvFileExists():
+    if not os.path.exists(".env"):
+        errorMessage(".env file not found. Please create a .env file based on the .env.example file and add the required environment variables.")
+
+def loadEnvironmentVariables():
+    dotenv.load_dotenv()
+    existingVars = ["GITHUB_TOKEN"]
+    for var in existingVars:
+        if var not in os.environ:
+            errorMessage(f"{var} environment variable not set within .env file")
+
+def errorMessage(message):
+    print(f"\033[91m{message}\033[0m")
+    sys.exit(1)
+
+if __name__ == '__main__':
+    checkDotEnvFileExists()
+    loadEnvironmentVariables()
+
+    print("SecAware - Currently Work in Progress")
