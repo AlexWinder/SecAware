ffa16f08032bee9b622172ad89ddcf8a9d0d59e5
Alex Winder <contact@alexwinder.com>
2026-02-20 21:45:37 +0200

    Building structure of dependencies

--- SecAware.py
+++ SecAware.py
@@ -8,9 +8,55 @@ import sys
 
 class SoftwareCompositionAnalysis:
     packages: dict
+    packageGraph: dict
 
     def __init__(self):
         self.packages = {}
+        self.packageGraph = {}
+
+    def generatePackageUrl(self, packageName, packageVersion):
+        return f"pkg:packagist/{packageName}@{packageVersion}"
+
+    def populateDataFromPackageLockFile(self, lockFilePath="composer.lock"):
+        with open(lockFilePath, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+        # Combine all production and development dependencies into a single list
+        allDependencies = data.get('packages', []) + data.get('packages-dev', [])
+        
+        # Require packages don't include the specific locked version, so we need a lookup
+        nameAndVersion = {package['name']: package['version'] for package in allDependencies}
+
+        for dependency in allDependencies:
+            name = dependency['name']
+            version = dependency['version']
+            packageUrl = self.generatePackageUrl(name, version)
+
+            # Add the package to the internal data structure
+            self.packages[packageUrl] = {
+                'name': name,
+                'version': version,
+                'ecosystem': 'Packagist',
+                'vulnerabilities' : []
+            }
+
+            self.packageGraph[packageUrl] = []
+
+            # Build the dependency graph
+            coreRequirements = dependency.get('require', {})
+            for coreName in coreRequirements:
+                # Strip out any platform requirements (e.g. php, ext-*)
+                if coreName.startswith("php") or coreName.startswith("ext-"):
+                    continue
+                
+                # Find what version of this dependency is actually used in the lock file
+                coreVersion = nameAndVersion.get(coreName)
+                if coreVersion:
+                    subDependencyPackageUrl = self.generatePackageUrl(coreName, coreVersion)
+                    self.packageGraph[packageUrl].append(subDependencyPackageUrl)
+
+        dumpJsonToFile("dependencies.json", self.packages)
+        dumpJsonToFile("dependencyGraph.json", self.packageGraph)
 
     def getKnownCVEsForPackageVersion(self, packageName, packageVersion):
         print(f"Identifying known CVEs for package: {packageName} {packageVersion}")
@@ -70,6 +116,4 @@ if __name__ == '__main__':
 
     print("SecAware - Currently Work in Progress")
 
-    sca.getKnownCVEsForPackageVersion("librenms/librenms", "26.0.0")
-
-    print(json.dumps(sca.packages, indent=2))
+    sca.populateDataFromPackageLockFile()
