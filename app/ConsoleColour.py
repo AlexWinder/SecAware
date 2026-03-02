2155f3a1a2d2d8b09dae89237675b92e90af251b
Alex Winder <contact@alexwinder.com>
2026-03-02 21:50:05 +0200

    Added `ConsoleColour` class

---
+++ app/ConsoleColour.py
@@ -0,0 +1,9 @@
+class ConsoleColour:
+    def toRed(text):
+        return f"\033[91m{text}\033[0m"
+
+    def toGreen(text):
+        return f"\033[92m{text}\033[0m"
+    
+    def toYellow(text):
+        return f"\033[93m{text}\033[0m"
