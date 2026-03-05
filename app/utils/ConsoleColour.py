7f7e0ac1b61f2e1800ff79f3a8726d752daffdc1
Alex Winder <contact@alexwinder.com>
2026-03-05 10:04:40 +0200

    Added `toBlue`

--- app/utils/ConsoleColour.py
+++ app/utils/ConsoleColour.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 class ConsoleColour:
+    def toGreen(text):
+        return f"\033[92m{text}\033[0m"
+
     def toRed(text):
         return f"\033[91m{text}\033[0m"
 
-    def toGreen(text):
-        return f"\033[92m{text}\033[0m"
+    def toBlue(text):
+        return f"\033[94m{text}\033[0m"
     
     def toYellow(text):
         return f"\033[93m{text}\033[0m"
+        
